163e7778303ecf09edec66151c93a1e03bb13967
sengleapheng65-lab <sengleapheng65@gmail.com>
2026-02-13 07:37:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const yesButton = document.querySelector(".yes");
 const noButton = document.querySelector(".no");
 const text = document.querySelector("h2");
 
-noButton.addEventListener("pointerenter", function () {
-  function getRandomNumber() {
-    return Math.floor(Math.random() * 301);
-  }
+noButton.addEventListener("pointerenter", moveButton);
 
-  noButton.style.left = getRandomNumber() + "px";
-  noButton.style.top = getRandomNumber() + "px";
-});
+function moveButton() {
+  const maxX = window.innerWidth - noButton.offsetWidth;
+  const maxY = window.innerHeight - noButton.offsetHeight;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noButton.style.left = randomX + "px";
+  noButton.style.top = randomY + "px";
+}
 
 yesButton.onclick = () =>
   (text.innerText = "I LOVE YOU TOO O SML HOUY HOUY !🥰😘💖");
